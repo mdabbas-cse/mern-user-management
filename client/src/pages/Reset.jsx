9d39b62dd43675dda0b87c5ef3780bddc0a9e45d
mdabbas-cse <gmabbas44@gmail.com>
2023-07-10 01:36:00 +0600

    update: reset password UI

--- client/src/pages/Reset.jsx
+++ client/src/pages/Reset.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { useFormik } from 'formik';
+import React from 'react';
+import styles from '../styles/user.module.css';
+import { resetPasswordValidation } from '../helpers/validate';
 
 export default function Reset() {
+
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      confirm_pwd: ''
+    },
+    validate: resetPasswordValidation,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async values => {
+      console.log(values);
+
+    }
+  })
+
   return (
-    <div>Reset</div>
+    <div className="container mx-auto">
+
+      <div className='flex justify-center items-center h-screen'>
+        <div className={styles.glass} style={{ width: "40%" }}>
+
+          <div className="title flex flex-col items-center">
+            <h4 className='text-5xl font-bold'>Reset</h4>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+              Enter new password.
+            </span>
+          </div>
+
+          <form className='py-20' onSubmit={formik.handleSubmit}>
+            <div className="textbox flex flex-col items-center gap-6">
+              <input
+                {...formik.getFieldProps('password')}
+                className={styles.textbox}
+                type="text"
+                placeholder='New Password'
+              />
+              <input
+                {...formik.getFieldProps('confirm_pwd')}
+                className={styles.textbox}
+                type="text"
+                placeholder='Repeat Password'
+              />
+              <button className={styles.btn} type='submit'>Reset</button>
+            </div>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
   )
 }
